ccbeb3147da29f968b984b9f42fe351f0c138717
joshuastewart417 <joshua.stewart780@gmail.com>
2021-08-20 15:05:52 -0500

    further styling and working on filtering array

--- scripts/fish/FishData.js
+++ scripts/fish/FishData.js
@@ -2,43 +2,130 @@
 
 const fishCollection = [
   {
-    name: "Dude",
-    diet: "hot dogs",
-    image: "clownfish.jpg",
-    species: "fella",
-    length: 8,
+    species: "Discus (Symphysodon discus)",
+    image: "discus.jpg",
+    name: "Theodore",
+    diet: "Omnivore",
+    length: "8",
     waterType: "Salt",
-    harvestLocation: "Australia",
+    harvestLocation: "Rio Negro, Amazon",
   },
   {
-    name: "Dudette",
-    diet: "cold dogs",
-    image: "clownfish.jpg",
-    species: "chica",
-    length: 6,
-    waterType: "Brackish",
-    harvestLocation: "Detroit",
-  },
-  {
-    name: "Duderino",
-    diet: "just dogs",
-    image: "clownfish.jpg",
-    species: "chile",
-    length: 5,
-    waterType: "Fresh",
-    harvestLocation: "Kentucky",
-  },
-  {
-    name: "Duderina",
-    diet: "warm dogs",
-    image: "clownfish.jpg",
-    species: "hoss",
-    length: 10,
+    species: "Wolf Cichlid (Parachromis dovii)",
+    image: "wolfcichlid.jpg",
+    name: "Fido",
+    diet: "Carnivore",
+    length: "30",
     waterType: "Salt",
-    harvestLocation: "Lebanon",
+    harvestLocation: "Costa Rica",
+  },
+  {
+    species: "Flowerhorn Cichlid (Paraneetroplus synspilus)",
+    image: "flowerhorncichlid.jpg",
+    name: "Daisy",
+    diet: "Carnivore",
+    length: "10",
+    waterType: "Salt",
+    harvestLocation: "Captivity",
+  },
+  {
+    species: "Zebra Pleco (Hypancistrus zebra)",
+    image: "zebrapleco.jpg",
+    name: "Stripes",
+    diet: "Omnivore",
+    length: "4",
+    waterType: "Salt",
+    harvestLocation: "Rio Xingu, Brazil",
+  },
+  {
+    species: "Black Ghost Knifefish (Apteronotus albifrons)",
+    image: "blackghostknifefish.jpg",
+    name: "Shadow",
+    diet: "Carnivore",
+    length: "20",
+    waterType: "Salt",
+    harvestLocation: "South America",
+  },
+  {
+    species: "Arowana (Scleropages formosus)",
+    image: "silverarowana.jpg",
+    name: "Powder",
+    diet: "Carnivore",
+    length: "48",
+    waterType: "Salt",
+    harvestLocation: "Amazon River, Brazil",
+  },
+  {
+    species: "Butterfly Fish (Chaetodontidae)",
+    image: "butterflyfish.jpg",
+    name: "Butter",
+    diet: "Omnivore",
+    length: "4",
+    waterType: "Salt",
+    harvestLocation: "Atlantic Ocean",
+  },
+  {
+    species: "Clown Fish (Amphiriprioninge)",
+    image: "clownfish.jpg",
+    name: "Nemo",
+    diet: "Omnivore",
+    length: "3",
+    waterType: "Salt",
+    harvestLocation: "Great Barrier Reef, Australia",
+  },
+  {
+    species: "Moray eel (Muraenidae)",
+    image: "morayeel.jpg",
+    name: "Slippy",
+    diet: "Carnivore",
+    length: "62",
+    waterType: "Salt",
+    harvestLocation: "South Pacific, Philippines",
+  },
+  {
+    species: "Puffer fish (Tetraodontitae)",
+    image: "pufferfish.jpg",
+    name: "Spike",
+    diet: "Omnivore",
+    length: "24",
+    waterType: "Salt",
+    harvestLocation: "Japan",
   },
 ];
 
 export const getFish = () => {
   return fishCollection;
+};
+
+export const getMostHolyFish = () => {
+  const holyFishArray = [];
+
+  for (const holy of fishCollection) {
+    if (holy.length % 3 === 0) {
+      holyFishArray.push(holy);
+    }
+  }
+  return holyFishArray;
+};
+
+export const getSoldierFish = () => {
+  const soldierFishArray = [];
+
+  for (const soldier of fishCollection) {
+    if (soldier.length % 5 === 0) {
+      soldierFishArray.push(soldier);
+    }
+  }
+  return soldierFishArray;
+};
+
+export const getUnworthy = () => {
+  const unworthyArray = [];
+
+  for (const unworthy of fishCollection) {
+    if (unworthy.length % 3 != 0 && unworthy.length % 5 != 0) {
+      unworthyArray.push(unworthy);
+    }
+  }
+  return unworthyArray;
 };
--- scripts/tips/Tip.js
+++ scripts/tips/Tip.js
@@ -1,5 +1,5 @@
 export const tipCard = (tipObj) => {
   return `<article class="tip-card">
-				<p>"${tipObj.body}"</p>
+				<p>${tipObj.body}</p>
         	</article>`;
 };
